c1936b5f936afc8d48bffebb63edd543e65fe297
Alex Maltsev <alexq.dev@gmail.com>
2018-07-27 02:30:44 +0300

    + jquery mask

--- src/vendor_entries/vendor.js
+++ src/vendor_entries/vendor.js
@@ -7,4 +7,5 @@ module.exports = [
   './node_modules/select2/dist/js/select2.js',
   './node_modules/slick-carousel/slick/slick.js',
   './src/vendor_entries/jquery-ui.min.js',
+  './node_modules/jquery-mask-plugin/dist/jquery.mask.min.js',
 ];
