dc20697be373f43f3d75f21c1d901055ce96cf65
Alex Maltsev <alexq.dev@gmail.com>
2018-07-27 04:42:39 +0300

    + interested block

--- src/vendor_entries/vendor.js
+++ src/vendor_entries/vendor.js
@@ -5,12 +5,11 @@
 module.exports = [
   './node_modules/jquery/dist/jquery.js',
   './node_modules/select2/dist/js/select2.js',
+  './node_modules/dragscroll/dragscroll.js',
   './node_modules/slick-carousel/slick/slick.js',
   './src/vendor_entries/jquery-ui.min.js',
   './node_modules/jquery-mask-plugin/dist/jquery.mask.min.js',
-  './node_modules/fancybox/lib/jquery.mousewheel.pack.js',
-  // './node_modules/fancybox/dist/js/jquery.fancybox.js',
   './src/vendor_entries/jquery.fancybox.js',
   './src/vendor_entries/jquery.fancybox-thumbs.js',
-  // './node_modules/fancybox/dist/helpers/js/jquery.fancybox-thumbs.js',
+  './src/vendor_entries/jquery.ui.touch-punch.min.js',
 ];
